14f849e10023c047e5cb1d4e444cef9989ccdb27
Samim Zafar <camim.xafar500@gmail.com>
2022-12-31 18:13:25 +0500

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,21 +7,30 @@ const fetchData = async (id) => {
   const { data } = await axios.get(`https://gorest.co.in/public/v1/posts?page=${id}`);
   return data;
  } catch (error) {
-  console.log("🚀 ~ file: Home.js:11 ~ fetchData ~ error", error);
-  throw Error(error);
- }
+  throw new Error(error);
+ };
+
 };
 const Home = () => {
  const { id } = useParams();
  const navigate = useNavigate();
  const pageID = parseInt(id);
  const toast = useToast();
- const { data, isLoading } = useQuery(['posts', pageID], () => fetchData(pageID), {
+ const { data, isLoading, isError, error } = useQuery(['posts', pageID], () => fetchData(pageID), {
   keepPreviousData: true,
   onError: (error) => {
    toast({ status: "error", title: error });
   }
  });
+
+ if (isLoading) {
+  console.log("🚀 ~ file: Home.js:29 ~ Home ~ isLoading", isLoading);
+  return <Grid placeItems="center" height="100vh"><Spinner /></Grid>;
+ }
+ else if (isError) {
+  console.log("🚀 ~ file: Home.js:29 ~ Home ~ isError", error);
+  return <h1>{error.message}</h1>;
+ }
  return (
   <Container maxW="1300px" mt="4">
    <Flex justify="space-between" mb="4">
@@ -35,18 +44,17 @@ const Home = () => {
     <Text>Current Page : {pageID}</Text>
     <Button colorScheme="green" onClick={() => navigate(`/posts/${pageID + 1}`)}>Next</Button>
    </Flex>
-   {isLoading ? <Grid placeItems="center" height="100vh"><Spinner /></Grid> :
-    data.data?.map(post => (
-     <Stack p="4" boxShadow="md" borderRadius="x1" border="1px solid #ccc" key={post.id} mb="4">
-      <Flex justify="space-between" >
-       <Text>UserID : {post.user_id}</Text>
-       <Text>PostID : {post.id}</Text>
-      </Flex>
-      <Heading>Title {post.title}</Heading>
-      <Text>Content {post.body}</Text>
-      <Button colorScheme="green" onClick={() => navigate(`/postDetail/${post.id}`, { state: post })} >View Deails</Button>
-     </Stack>
-    ))
+   {data.data?.map(post => (
+    <Stack p="4" boxShadow="md" borderRadius="x1" border="1px solid #ccc" key={post.id} mb="4">
+     <Flex justify="space-between" >
+      <Text>UserID : {post.user_id}</Text>
+      <Text>PostID : {post.id}</Text>
+     </Flex>
+     <Heading>Title {post.title}</Heading>
+     <Text>Content {post.body}</Text>
+     <Button colorScheme="green" onClick={() => navigate(`/postDetail/${post.id}`, { state: post })} >View Deails</Button>
+    </Stack>
+   ))
    }
   </Container>
  );
