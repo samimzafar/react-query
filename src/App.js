450ccca9d82ed13d289acbbf43523d526a8e940c
Samim Zafar <camim.xafar500@gmail.com>
2022-12-31 11:29:00 +0500

    useLocation hook added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, Heading } from '@chakra-ui/react';
 import { Route, Routes } from "react-router-dom";
 import { ReactQueryDevtools } from 'react-query/devtools';
-import Home from "./Pages/Home";
+import {PostDetails,Home} from "./Pages";
 const App = () => {
  // Create a client
  const queryClient = new QueryClient();
@@ -10,7 +10,9 @@ const App = () => {
   <ChakraProvider>
    <QueryClientProvider client={queryClient}>
     <Routes>
-     <Route path="/:id" element={<Home />} />
+     <Route exact path="/posts/:id" element={<Home />} />
+     <Route path="/postDetail/:id" element={<PostDetails />} />
+     <Route path="*" element={<Heading>Page not found!</Heading>} />
     </Routes>
     <ReactQueryDevtools initialIsOpen={false} />
    </QueryClientProvider>
---
+++ src/Pages/index.js
@@ -0,0 +1,2 @@
+export {default as Home} from "./Home"
+export {default as PostDetails} from "./PostDetails"
